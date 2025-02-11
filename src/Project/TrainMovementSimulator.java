33e34433637c375108f65e0f42fc4beb67592751
MarkL-UCF <ma999600@ucf.edu>
2025-02-11 09:11:30 -0500

    First part of the skeleton

---
+++ src/Project/TrainMovementSimulator.java
@@ -0,0 +1,92 @@
+package Project;
+
+import java.util.*;
+import java.io.*;
+
+public class TrainMovementSimulator {
+    static final int MAXTRAINS = 30; //max number of trains to be routed through the train yard
+    static final int MAXALIGNMENTS = 60; //max number of track to switch combinations in the train yard
+    static final int MAXSWITCHES = 10; //maximum number of switches in the train yard
+
+    public static void main(String[] args) throws InterruptedException {
+        int numberOfTrainsInTheSimulationFleet;
+        int numberOfPathsThroughTheTrainYard;
+
+        //variables holding the details of the trains
+
+
+
+        //variables holding the details of the train yard
+
+
+        //theFleet contains all the trains in the simulation run that need to move through the train yard
+        //Train[] theFleet = new Train[MAXTRAINS];
+
+
+        //define the file, file reader, buffered reader, and scanner objects needed
+        File theFleetFile = new File("theFleetFile.csv"); //info on the trains in the sim
+        File theYardFile = new File("theYardFile.csv"); //info on the configuration of the yard
+
+        try {
+            System.out.println("\n Spring 2025 - Project 2 - Train Movement Simulator \n\n");
+            System.out.println("\n * * * * * * * * * * INITIALIZATION OF SIMULATION BEGINS * * * * * * * * * * \n");
+            System.out.println("\n\n");
+
+            //read in the theFleet.csv file
+            //the number of lines in this file indicates the number of trains in the fleet
+            //this file contains the info on the trains in the fleet schedule to be simulated
+            //the file is a comma separated file with information on each train in the form of
+            //train number (int), inbound track (int), outbound track (int)
+            //ex: 1,6,4
+
+            //DEBUGGING BLOCK
+            System.out.println("\nDetails of the Train Fleet being simulated this run\n");
+            System.out.println("Train number            Inbound Track           Outbound Track          Hold            Dispatched          Dispatch Sequence");
+            System.out.println("------------------------------------------------------------------------------------------------------------");
+            //END DEBUGGING BLOCK
+
+            //read a train's details
+
+            //DEBUGGING BLOCK
+            //System.out.println(" " + trainNumber + "\t\t\t" + inboundTrack + "\t\t\t" + outboundTrack + "\t\t\t" + hold + "\t\t\t" + dispatched + "\t\t\t" + dispatchSequence);
+            //System.out.println();
+
+            //Create this train object
+
+            //Get the next line of train data from theFleetFile
+
+            //Read in the theYardConfiguration.csv File
+            //this file contains the information on the train yard configuration. This is how dispatcher knows
+            //which tracks are connected to which switches so that trains can be properly routed through the train yard
+            //this file is a comma separated file with information on each track/switch connection
+            //each line has the form:
+            //inbound track number (int), first switch number (int), second switch number (int), third switch number (int), outbound track number (int)
+            //example: 1,1,3,4,1
+            //read theFleetFile into an array and get the number of lines in the file as well to represent the fleetSize
+
+            //DEBUGGING BLOCK
+
+            //END DEBUGGING BLOCK
+
+            //read the yard details -
+
+            //DEBUGGING BLOCK
+            //END DEBUGGING BLOCK
+
+            //create the switch objects
+
+            //make the assignment of switches for each train in the simulation fleet
+
+
+            //DEBUGGING BLOCK
+
+            //END DEBUGGING BLOCK
+
+        } //end try block
+        catch(Exception e){
+            e.printStackTrace();
+        } //end catch block
+
+
+    }
+}
