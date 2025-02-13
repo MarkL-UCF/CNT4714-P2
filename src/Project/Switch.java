c41979a3b62592d79284c871ef2d9294583162ae
MarkL-UCF <ma999600@ucf.edu>
2025-02-13 14:28:04 -0500

    locks

--- src/Project/Switch.java
+++ src/Project/Switch.java
@@ -1,16 +1,19 @@
 package Project;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 //this class implements the sharable/lockable/synchronized
 //switch objects shared by the trains moving through the train yard
 public class Switch {
     //define class variables as needed
     protected int switchNum;
-
+    private ReentrantLock lock;
 
     //constructor method
     //switch objects
     public Switch(int switchNumArg) {
         this.switchNum = switchNumArg;
+        this.lock = new ReentrantLock();
     }
 
     //method for trains to acquire the switch locks
